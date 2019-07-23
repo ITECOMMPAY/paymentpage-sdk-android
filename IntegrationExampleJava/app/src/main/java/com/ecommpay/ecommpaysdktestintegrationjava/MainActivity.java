9a23efc8236a8442fadd169c5901cc6291137984
Виталий Морозов <gallifrey46@gmail.com>
2019-07-23 16:27:23 +0300

    v1.1

--- IntegrationExampleJava/app/src/main/java/com/ecommpay/ecommpaysdktestintegrationjava/MainActivity.java
+++ IntegrationExampleJava/app/src/main/java/com/ecommpay/ecommpaysdktestintegrationjava/MainActivity.java
@@ -5,9 +5,10 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.ecommpay.sdk.components.ECMPActivity;
-import com.ecommpay.sdk.entities.ECMPPaymentInfo;
-import com.ecommpay.sdk.entities.ECMPRecurrentInfo;
+import com.ecommpay.sdk.ECMPActivity;
+import com.ecommpay.sdk.ECMPAdditionalField;
+import com.ecommpay.sdk.ECMPPaymentInfo;
+import com.ecommpay.sdk.ECMPRecurrentInfo;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,9 +30,16 @@ public class MainActivity extends AppCompatActivity {
         // Sign payment info
         paymentInfo.setSignature(signature);
 
+        // Set theme
+//        ECMPTheme theme = ECMPTheme.getDarkTheme();
+//        theme.fullScreenBackgroundColor = Color.GREEN;
+//        theme.showShadow = false;
+
         // Present Checkout UI
         startActivityForResult(ECMPActivity.buildIntent(this,
-                paymentInfo),
+                paymentInfo
+//                ,theme
+                ),
                 PAY_ACTIVITY_REQUEST);
     }
 
@@ -40,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == PAY_ACTIVITY_REQUEST) {
+
             switch (resultCode) {
                 case ECMPActivity.RESULT_SUCCESS:
                 case ECMPActivity.RESULT_CANCELLED:
@@ -48,8 +57,12 @@ public class MainActivity extends AppCompatActivity {
                     break;
             }
 
-            if(data != null && data.hasExtra(ECMPActivity.DATA_INTENT_EXTRA)) {
-                String message = data.getStringExtra(ECMPActivity.DATA_INTENT_EXTRA);
+            if(data != null && data.hasExtra(ECMPActivity.DATA_INTENT_EXTRA_ERROR)) {
+                String error = data.getStringExtra(ECMPActivity.DATA_INTENT_EXTRA_ERROR);
+            }
+
+            if(data != null && data.hasExtra(ECMPActivity.DATA_INTENT_EXTRA_TOKEN)) {
+                String token = data.getStringExtra(ECMPActivity.DATA_INTENT_EXTRA_TOKEN);
             }
         }
     }
@@ -78,7 +91,15 @@ public class MainActivity extends AppCompatActivity {
 
     // Additional
     void setDMSPayment(ECMPPaymentInfo paymentInfo) {
-        paymentInfo.setCreditCardPaymentType(ECMPPaymentInfo.CreditCardPaymentType.Auth);
+        paymentInfo.setAction(ECMPPaymentInfo.ActionType.Auth);
+    }
+
+    void setActionTokenize(ECMPPaymentInfo paymentInfo) {
+        paymentInfo.setAction(ECMPPaymentInfo.ActionType.Tokenize);
+    }
+
+    void setActionVerify(ECMPPaymentInfo paymentInfo) {
+        paymentInfo.setAction(ECMPPaymentInfo.ActionType.Verify);
     }
 
     void setToken(ECMPPaymentInfo paymentInfo) {
@@ -88,12 +109,27 @@ public class MainActivity extends AppCompatActivity {
     void setRecurrent(ECMPPaymentInfo paymentInfo) {
         ECMPRecurrentInfo recurrentInfo = new ECMPRecurrentInfo(
                 "R", // type
+                "20", // expiry day
                 "11", // expiry month
                 "2030", // expiry year
                 "M", // recurrent period
                 "12:00:00", // start time
                 "12-02-2020", // start date
                 "your_recurrent_id"); // recurrent payment ID
+        // Additional options if needed
+//        recurrentInfo.setAmount(1000);
+//        recurrentInfo.setSchedule(new ECMPRecurrentInfoSchedule[]{
+//                new ECMPRecurrentInfoSchedule("20-10-2020",1000),
+//                new ECMPRecurrentInfoSchedule("20-10-2020",1000)
+//        });
+
         paymentInfo.setRecurrent(recurrentInfo);
+    }
+
+    void setKnownAdditionalFields(ECMPPaymentInfo paymentInfo) {
+        paymentInfo.setECMPAdditionalFields(new ECMPAdditionalField[]{
+                new ECMPAdditionalField(ECMPAdditionalField.AdditionalFieldType.customer_first_name, "Mark"),
+                new ECMPAdditionalField(ECMPAdditionalField.AdditionalFieldType.billing_country, "US"),
+        });
     }
 }
--- IntegrationExampleKotlin/app/src/main/java/com/ecommpay/test_integration_kotlin/MainActivity.kt
+++ IntegrationExampleKotlin/app/src/main/java/com/ecommpay/test_integration_kotlin/MainActivity.kt
@@ -2,11 +2,8 @@ package com.ecommpay.test_integration_kotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import com.ecommpay.sdk.components.ECMPActivity
-import com.ecommpay.sdk.entities.ECMPPaymentInfo
 import android.content.Intent
-import com.ecommpay.sdk.entities.ECMPRecurrentInfo
-
+import com.ecommpay.sdk.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -27,11 +24,17 @@ class MainActivity : AppCompatActivity() {
         // Sign payment info
         paymentInfo.signature = signature
 
+        // Set theme
+//        val theme = ECMPTheme.getDarkTheme()
+//        theme.fullScreenBackgroundColor = Color.GREEN
+//        theme.showShadow = false
+
         // Present Checkout UI
         startActivityForResult(
             ECMPActivity.buildIntent(
                 this,
                 paymentInfo
+//              ,theme
             ),
             PAY_ACTIVITY_REQUEST
         )
@@ -47,7 +50,8 @@ class MainActivity : AppCompatActivity() {
                 ECMPActivity.RESULT_DECLINE -> {}
                 ECMPActivity.RESULT_FAILED -> {}
             }
-            val message = data?.getStringExtra(ECMPActivity.DATA_INTENT_EXTRA)
+            val error = data?.getStringExtra(ECMPActivity.DATA_INTENT_EXTRA_ERROR)
+            val token = data?.getStringExtra(ECMPActivity.DATA_INTENT_EXTRA_TOKEN)
         }
     }
 
@@ -75,7 +79,15 @@ class MainActivity : AppCompatActivity() {
 
     // Additional
     internal fun setDMSPayment(paymentInfo: ECMPPaymentInfo) {
-        paymentInfo.creditCardPaymentType = ECMPPaymentInfo.CreditCardPaymentType.Auth
+        paymentInfo.setAction(ECMPPaymentInfo.ActionType.Auth)
+    }
+
+    internal fun setActionTokenize(paymentInfo: ECMPPaymentInfo) {
+        paymentInfo.setAction(ECMPPaymentInfo.ActionType.Tokenize)
+    }
+
+    internal fun setActionVerify(paymentInfo: ECMPPaymentInfo) {
+        paymentInfo.setAction(ECMPPaymentInfo.ActionType.Verify)
     }
 
     internal fun setToken(paymentInfo: ECMPPaymentInfo) {
@@ -85,12 +97,29 @@ class MainActivity : AppCompatActivity() {
     internal fun setRecurrent(paymentInfo: ECMPPaymentInfo) {
         val recurrentInfo = ECMPRecurrentInfo(
             "R", // type
+            "20", // expiry day
             "11", // expiry month
             "2030", // expiry year
             "M", // recurrent period
             "12:00:00", // start time
             "12-02-2020", // start date
             "your_recurrent_id") // recurrent payment ID
+        // Additional options if needed
+//        recurrentInfo.setAmount(1000)
+//        recurrentInfo.setSchedule(
+//            arrayOf(
+//                ECMPRecurrentInfoSchedule("20-10-2020", 1000),
+//                ECMPRecurrentInfoSchedule("20-10-2020", 1000)
+//            )
+//        )
+
         paymentInfo.setRecurrent(recurrentInfo)
+    }
+
+    internal fun setKnownAdditionalFields(paymentInfo: ECMPPaymentInfo) {
+        paymentInfo.ecmpAdditionalFields = arrayOf(
+            ECMPAdditionalField(ECMPAdditionalField.AdditionalFieldType.customer_first_name, "Mark"),
+            ECMPAdditionalField(ECMPAdditionalField.AdditionalFieldType.billing_country, "US")
+        )
     }
 }
