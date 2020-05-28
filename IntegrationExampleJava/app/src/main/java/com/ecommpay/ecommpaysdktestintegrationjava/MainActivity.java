09ea29a6bf860342478ebc03a0ed8cbca4603072
Виталий Морозов <v.morozov@it.ecommpay.com>
2020-05-28 15:24:09 +0300

    v1.5

--- IntegrationExampleJava/app/src/main/java/com/ecommpay/ecommpaysdktestintegrationjava/MainActivity.java
+++ IntegrationExampleJava/app/src/main/java/com/ecommpay/ecommpaysdktestintegrationjava/MainActivity.java
@@ -10,6 +10,13 @@ import com.ecommpay.sdk.ECMPAdditionalField;
 import com.ecommpay.sdk.ECMPAdditionalFieldEnums;
 import com.ecommpay.sdk.ECMPPaymentInfo;
 import com.ecommpay.sdk.ECMPRecurrentInfo;
+import com.ecommpay.sdk.threeDSecure.ECMPThreeDSecureAccountInfo;
+import com.ecommpay.sdk.threeDSecure.ECMPThreeDSecureCustomerInfo;
+import com.ecommpay.sdk.threeDSecure.ECMPThreeDSecureGiftCardInfo;
+import com.ecommpay.sdk.threeDSecure.ECMPThreeDSecureInfo;
+import com.ecommpay.sdk.threeDSecure.ECMPThreeDSecureMpiResultInfo;
+import com.ecommpay.sdk.threeDSecure.ECMPThreeDSecurePaymentInfo;
+import com.ecommpay.sdk.threeDSecure.ECMPThreeDSecureShippingInfo;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -137,5 +144,89 @@ public class MainActivity extends AppCompatActivity {
                 new ECMPAdditionalField(ECMPAdditionalFieldEnums.AdditionalFieldType.customer_first_name, "Mark"),
                 new ECMPAdditionalField(ECMPAdditionalFieldEnums.AdditionalFieldType.billing_country, "US"),
         });
+    }
+
+    // Setup 3D Secure 2.0 parameters
+    void setThreeDSecureParams(ECMPPaymentInfo paymentInfo) {
+        ECMPThreeDSecureInfo threeDSecureInfo = new ECMPThreeDSecureInfo();
+
+        ECMPThreeDSecurePaymentInfo threeDSecurePaymentInfo = new ECMPThreeDSecurePaymentInfo();
+
+        threeDSecurePaymentInfo
+                .setReorder("01") // This parameter indicates whether the cardholder is reordering previously purchased merchandise.
+                .setPreorderPurchase("01") // This parameter indicates whether cardholder is placing an order for merchandise with a future availability or release date.
+                .setPreorderDate("01-10-2019") // The date the preordered merchandise will be available.Format: dd-mm-yyyy.
+                .setChallengeIndicator("01") // This parameter indicates whether challenge flow is requested for this payment.
+                .setChallengeWindow("01"); // The dimensions of a window in which authentication page opens.
+
+        ECMPThreeDSecureGiftCardInfo threeDSecureGiftCardInfo = new ECMPThreeDSecureGiftCardInfo();
+
+        threeDSecureGiftCardInfo
+                .setAmount(12345) // Amount of payment with prepaid or gift card denominated in minor currency units.
+                .setCurrency("USD") // Currency of payment with prepaid or gift card in the ISO 4217 alpha-3 format
+                .setCount(1); // Total number of individual prepaid or gift cards/codes used in purchase.
+
+        threeDSecurePaymentInfo.setGiftCard(threeDSecureGiftCardInfo);
+
+        ECMPThreeDSecureCustomerInfo threeDSecureCustomerInfo = new ECMPThreeDSecureCustomerInfo();
+        threeDSecureCustomerInfo
+                .setAddressMatch("Y") //The parameter indicates whether the customer billing address matches the address specified in the shipping object.
+                .setHomePhone("79105211111") // Customer home phone number.
+                .setWorkPhone("73141211111") // Customer work phone number.
+                .setBillingRegionCode("ABC"); // State, province, or region code in the ISO 3166-2 format. Example: SPE for Saint Petersburg, Russia.
+
+        ECMPThreeDSecureAccountInfo threeDSecureAccountInfo = new ECMPThreeDSecureAccountInfo();
+
+        threeDSecureAccountInfo
+                .setActivityDay(22) // Number of card payment attempts in the last 24 hours.
+                .setActivityYear(222) // Number of card payment attempts in the last 365 days.
+                .setAdditional("gamer12345") // Additional customer account information, for instance arbitrary customer ID.
+                .setAgeIndicator("01") // Number of days since the customer account was created.
+                .setDate("01-10-2019") // Account creation date.
+                .setChangeDate("01-10-2019") // Last account change date except for password change or password reset.
+                .setChangeIndicator("01") // Number of days since last customer account update, not including password change or reset.
+                .setPassChangeDate("01") // Last password change or password reset date.
+                .setPassChangeIndicator("01-10-2019") // Number of days since the last password change or reset.
+                .setProvisionAttempts(16) // Number of attempts to add card details in customer account in the last 24 hours.
+                .setPurchaseNumber(12) // Number of purchases with this cardholder account in the previous six months.
+                .setAuthData("login_0102") // Any additional log in information in free text.
+                .setAuthTime("01-10-2019 13:12") // Account log on date and time.
+                .setAuthMethod("01") // Authentication type the customer used to log on to the account when placing the order.
+                .setSuspiciousActivity("01") // Suspicious activity detection result.
+                .setPaymentAge("01-10-2019") // Card record creation date.
+                .setPaymentAgeIndicator("01"); //  Number of days since the payment card details were saved in a customer account.
+
+        ECMPThreeDSecureShippingInfo threeDSecureShippingInfo = new ECMPThreeDSecureShippingInfo();
+
+        threeDSecureShippingInfo
+                .setType("01") //Shipment indicator.
+                .setDeliveryTime("01") //Shipment terms.
+                .setDeliveryEmail("test@gmail.com") // The email to ship purchased digital content, if the customer chooses email delivery.
+                .setAddressUsageIndicator("01") // Number of days since the first time usage of the shipping address.
+                .setAddressUsage("01-10-2019") // First shipping address usage date in the dd-mm-yyyy format.
+                .setCity("Moscow") // Shipping city.
+                .setCountry("RU") // Shipping country in the ISO 3166-1 alpha-2 format
+                .setAddress("Lenina street 12") // Shipping address.
+                .setPostal("109111") // Shipping postbox number.
+                .setRegionCode("MOW") // State, province, or region code in the ISO 3166-2 format.
+                .setNameIndicator("01"); // Shipment recipient flag.
+
+        ECMPThreeDSecureMpiResultInfo threeDSecureMpiResultInfo = new ECMPThreeDSecureMpiResultInfo();
+
+        threeDSecureMpiResultInfo
+                .setAcsOperationId("321412-324-sda23-2341-adf12341234") // The ID the issuer assigned to the previous customer operation and returned in the acs_operation_id parameter inside the callback with payment processing result. Maximum 30 characters.
+                .setAuthenticationFlow("01") // The flow the issuer used to authenticate the cardholder in the previous operation and returned in the authentication_flow parameter of the callback with payment processing results.
+                .setAuthenticationTimestamp("21323412321324"); // Date and time of the previous successful customer authentication as returned in the mpi_timestamp parameter inside the callback message with payment processing result.
+
+        threeDSecureCustomerInfo
+                .setAccountInfo(threeDSecureAccountInfo)
+                .setMpiResultInfo(threeDSecureMpiResultInfo)
+                .setShippingInfo(threeDSecureShippingInfo);
+
+
+        threeDSecureInfo.setThreeDSecureCustomerInfo(threeDSecureCustomerInfo);
+        threeDSecureInfo.setThreeDSecurePaymentInfo(threeDSecurePaymentInfo);
+
+        paymentInfo.setEcmpThreeDSecureInfo(threeDSecureInfo);
     }
 }
--- IntegrationExampleKotlin/app/src/main/java/com/ecommpay/test_integration_kotlin/MainActivity.kt
+++ IntegrationExampleKotlin/app/src/main/java/com/ecommpay/test_integration_kotlin/MainActivity.kt
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
 import com.ecommpay.sdk.*
+import com.ecommpay.sdk.threeDSecure.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -125,5 +126,89 @@ class MainActivity : AppCompatActivity() {
             ECMPAdditionalField(ECMPAdditionalFieldEnums.AdditionalFieldType.customer_first_name, "Mark"),
             ECMPAdditionalField(ECMPAdditionalFieldEnums.AdditionalFieldType.billing_country, "US")
         )
+    }
+
+    // Setup 3D Secure parameters
+    internal fun setupThreeDSecureParams(paymentInfo: ECMPPaymentInfo) {
+        val threeDSecureInfo = ECMPThreeDSecureInfo()
+
+        val threeDSecurePaymentInfo = ECMPThreeDSecurePaymentInfo()
+
+        threeDSecurePaymentInfo
+            .setReorder("01") // This parameter indicates whether the cardholder is reordering previously purchased merchandise.
+            .setPreorderPurchase("01") // This parameter indicates whether cardholder is placing an order for merchandise with a future availability or release date.
+            .setPreorderDate("01-10-2019") // The date the preordered merchandise will be available.Format: dd-mm-yyyy.
+            .setChallengeIndicator("01") // This parameter indicates whether challenge flow is requested for this payment.
+            .challengeWindow = "01" // The dimensions of a window in which authentication page opens.
+
+        val threeDSecureGiftCardInfo = ECMPThreeDSecureGiftCardInfo()
+
+        threeDSecureGiftCardInfo
+            .setAmount(12345) // Amount of payment with prepaid or gift card denominated in minor currency units.
+            .setCurrency("USD") // Currency of payment with prepaid or gift card in the ISO 4217 alpha-3 format
+            .count = 1 // Total number of individual prepaid or gift cards/codes used in purchase.
+
+        threeDSecurePaymentInfo.giftCard = threeDSecureGiftCardInfo // object with information about payment with prepaid card or gift card.
+
+        val threeDSecureCustomerInfo = ECMPThreeDSecureCustomerInfo()
+        threeDSecureCustomerInfo
+            .setAddressMatch("Y") //The parameter indicates whether the customer billing address matches the address specified in the shipping object.
+            .setHomePhone("79105211111") // Customer home phone number.
+            .setWorkPhone("73141211111") // Customer work phone number.
+            .billingRegionCode = "ABC" // State, province, or region code in the ISO 3166-2 format. Example: SPE for Saint Petersburg, Russia.
+
+        val threeDSecureAccountInfo = ECMPThreeDSecureAccountInfo() // object with account information on record with merchant
+
+        threeDSecureAccountInfo
+            .setActivityDay(22) // Number of card payment attempts in the last 24 hours.
+            .setActivityYear(222) // Number of card payment attempts in the last 365 days.
+            .setAdditional("gamer12345") // Additional customer account information, for instance arbitrary customer ID.
+            .setAgeIndicator("01") // Number of days since the customer account was created.
+            .setDate("01-10-2019") // Account creation date.
+            .setChangeDate("01-10-2019") // Last account change date except for password change or password reset.
+            .setChangeIndicator("01") // Number of days since last customer account update, not including password change or reset.
+            .setPassChangeDate("01") // Last password change or password reset date.
+            .setPassChangeIndicator("01-10-2019") // Number of days since the last password change or reset.
+            .setProvisionAttempts(16) // Number of attempts to add card details in customer account in the last 24 hours.
+            .setPurchaseNumber(12) // Number of purchases with this cardholder account in the previous six months.
+            .setAuthData("login_0102") // Any additional log in information in free text.
+            .setAuthTime("01-10-2019 13:12") // Account log on date and time.
+            .setAuthMethod("01") // Authentication type the customer used to log on to the account when placing the order.
+            .setSuspiciousActivity("01") // Suspicious activity detection result.
+            .setPaymentAge("01-10-2019") // Card record creation date.
+            .paymentAgeIndicator = "01" //  Number of days since the payment card details were saved in a customer account.
+
+        val threeDSecureShippingInfo = ECMPThreeDSecureShippingInfo() // object that contains shipment details
+
+        threeDSecureShippingInfo
+            .setType("01") //Shipment indicator.
+            .setDeliveryTime("01") //Shipment terms.
+            .setDeliveryEmail("test@gmail.com") // The email to ship purchased digital content, if the customer chooses email delivery.
+            .setAddressUsageIndicator("01") // Number of days since the first time usage of the shipping address.
+            .setAddressUsage("01-10-2019") // First shipping address usage date in the dd-mm-yyyy format.
+            .setCity("Moscow") // Shipping city.
+            .setCountry("RU") // Shipping country in the ISO 3166-1 alpha-2 format
+            .setAddress("Lenina street 12") // Shipping address.
+            .setPostal("109111") // Shipping postbox number.
+            .setRegionCode("MOW") // State, province, or region code in the ISO 3166-2 format.
+            .nameIndicator = "01" // Shipment recipient flag.
+
+        val threeDSecureMpiResultInfo = ECMPThreeDSecureMpiResultInfo() // object that contains information about previous customer authentication
+
+        threeDSecureMpiResultInfo
+            .setAcsOperationId("321412-324-sda23-2341-adf12341234") // The ID the issuer assigned to the previous customer operation and returned in the acs_operation_id parameter inside the callback with payment processing result. Maximum 30 characters.
+            .setAuthenticationFlow("01") // The flow the issuer used to authenticate the cardholder in the previous operation and returned in the authentication_flow parameter of the callback with payment processing results.
+            .authenticationTimestamp = "21323412321324" // Date and time of the previous successful customer authentication as returned in the mpi_timestamp parameter inside the callback message with payment processing result.
+
+        threeDSecureCustomerInfo
+            .setAccountInfo(threeDSecureAccountInfo) // object with account information on record with merchant
+            .setMpiResultInfo(threeDSecureMpiResultInfo) // object that contains information about previous customer authentication
+            .shippingInfo = threeDSecureShippingInfo // object that contains shipment details
+
+
+        threeDSecureInfo.threeDSecureCustomerInfo = threeDSecureCustomerInfo
+        threeDSecureInfo.threeDSecurePaymentInfo = threeDSecurePaymentInfo
+
+        paymentInfo.ecmpThreeDSecureInfo = threeDSecureInfo
     }
 }
